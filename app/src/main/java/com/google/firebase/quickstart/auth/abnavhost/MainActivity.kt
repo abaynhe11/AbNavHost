38e361383e66f7aa67198537f67aef0ca3d4ebeb
Abaynhe Abitew <abaynheabitew@yahoo.com>
2022-10-11 12:53:36 +0300

    Very simple Navigation action (Using fragment)

---
+++ app/src/main/java/com/google/firebase/quickstart/auth/abnavhost/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.google.firebase.quickstart.auth.abnavhost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
